333ec60b848da953f6e8de7077483193281d7da2
Chri_ <chrix94x2@gmail.com>
2020-11-20 18:09:07 +0100

    modified list and launcher now is able to run on rasp

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-exec python3 -u /main.py &
-exec python3 -u /amazon_bot/launcher.py
+exec python3 -u main.py &
+exec python3 -u amazon_bot/launcher.py
 #lanciare il launcher.py con il loop invece di amazonMain.py
